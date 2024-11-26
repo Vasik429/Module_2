31ad6104e4f0d63a7922d781bdec654a1edbe3f7
Vasik429 <v.404.akk@gmail.com>
2024-11-26 16:45:54 +0500

    if, else

---
+++ module_2_2.py
@@ -0,0 +1,26 @@
+first = 567
+second = 321
+third = 987
+print(first, second, third)
+if first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+first = 567
+second = 321
+third = 567
+print(first, second, third)
+if first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+first = 567
+second = 567
+third = 567
+print(first, second, third)
+if first == second or first == third or second == third:
+    print(3)
+else:
+    print(0)
