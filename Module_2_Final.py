06cd8fcbc92bf4630eca257772add6d93423975b
Vasik429 <v.404.akk@gmail.com>
2024-11-30 01:36:41 +0500

    Попытка найти решние на доп задачу 2 модуля 1

---
+++ Module_2_Final.py
@@ -0,0 +1,17 @@
+def save_key (key, values):
+    keys = {}
+    for key in range (3, 21):
+        for n in range (1, 21):
+            for r in range (20):
+                result = n + r
+                break
+        if result == key:
+            keys[key] = result
+        else:
+            pass
+keys =
+
+# for i in range (1, 21):
+#     for j in range (1, 20):
+#         print(i + j)
+
