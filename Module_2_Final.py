c4c255f211d31f2ebd84d064658323e384375a55
Vasik429 <v.404.akk@gmail.com>
2024-12-05 01:56:45 +0500

    варианты кода

--- Module_2_Final.py
+++ Module_2_Final.py
@@ -1,10 +1,24 @@
-for n in range (3, 21):
-    password = []
-    for i in range(1, 20):
-        for j in range(2, 20):
-            if n % (i + j):
-                while n >= (i + j) and n % (i + j):
-                    password.append(i)
-                    password.append(j)
-print(password)
+# def Password (number):
+#     pass_ = ''
+#     for i in range(1, number):
+#         for j in range(i + 1, number):
+#             if number % (i + j) == 0:
+#                 pass_ += str(i) + str(j)
+#     return pass_
+#
+# while 1 > 0:
+#     print(Password(int(input('Введите число от 3 до 20: '))))
+
+# numbers = set(range(3, 21))
+# print(*numbers)
+#
+# def find_pass (numbers):
+#     pass_ = ''
+#     for i in range(1, numbers):
+#         for j in range(i + 1, numbers):
+#             if numbers % (i + j) == 0:
+#                 pass_ += str(i) + str(j)
+#     return pass_
+#
+# find_pass(numbers)
 
