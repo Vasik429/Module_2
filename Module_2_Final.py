4d23bf93d39943bc49f84c4f9ca3947143b96c89
Vasik429 <v.404.akk@gmail.com>
2024-12-17 22:25:51 +0500

    Разобрали с преподавателем 2 варианта решения

--- Module_2_Final.py
+++ Module_2_Final.py
@@ -1,3 +1,5 @@
+import random
+
 def Password (number):
     pass_ = ''
     for i in range(1, number):
@@ -6,21 +8,20 @@ def Password (number):
                 pass_ += str(i) + str(j)
     return pass_
 
-
-result1 = Password(5)
+number = random.randint(3, 20)
+result1 = Password(number)
+print(number)
 print(result1)
 
 
-# num = set(range(3, 21))
-# print(num)
-#
-# def find_pass (*numbers):
-#     pass_ = ''
-#     for i in range(1, 21):
-#         for j in range(i + 1, numbers):
-#             if numbers % (i + j) == 0:
-#                 pass_ += str(i) + str(j)
-#     return pass_
-#
-# result2 = find_pass(num)
-# print(result2)
+def Password (number):
+    pass_ = ''
+    for i in range(1, number):
+        for j in range(i + 1, number):
+            if number % (i + j) == 0:
+                pass_ += str(i) + str(j)
+    return pass_
+k = 0
+while k < 5:
+    print(Password(int(input('Введите число от 3 до 20: '))))
+    k += 1
