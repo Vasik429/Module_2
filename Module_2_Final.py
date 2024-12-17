7775629347e54ab58fa35c4a0c37886d640c1cee
Vasik429 <v.404.akk@gmail.com>
2024-12-17 20:47:19 +0500

    убрала while

--- Module_2_Final.py
+++ Module_2_Final.py
@@ -1,24 +1,26 @@
-# def Password (number):
-#     pass_ = ''
-#     for i in range(1, number):
-#         for j in range(i + 1, number):
-#             if number % (i + j) == 0:
-#                 pass_ += str(i) + str(j)
-#     return pass_
-#
-# while 1 > 0:
-#     print(Password(int(input('Введите число от 3 до 20: '))))
+def Password (number):
+    pass_ = ''
+    for i in range(1, number):
+        for j in range(i + 1, number):
+            if number % (i + j) == 0:
+                pass_ += str(i) + str(j)
+    return pass_
 
-# numbers = set(range(3, 21))
-# print(*numbers)
+
+result1 = Password(5)
+print(result1)
+
+
+# num = set(range(3, 21))
+# print(num)
 #
-# def find_pass (numbers):
+# def find_pass (*numbers):
 #     pass_ = ''
-#     for i in range(1, numbers):
+#     for i in range(1, 21):
 #         for j in range(i + 1, numbers):
 #             if numbers % (i + j) == 0:
 #                 pass_ += str(i) + str(j)
 #     return pass_
 #
-# find_pass(numbers)
-
+# result2 = find_pass(num)
+# print(result2)
